20e04ebedd6c34f28816941b1dea0329f81036e2
Sundeepa Singh <56774147+ASI11006@users.noreply.github.com>
2024-02-18 18:48:43 +1100

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[4]:
+
+
+bom = pd.read_csv('Downloads/dsc-project-template-template-mvp (1)/dsc-project-template-template-mvp/zippedData/bom.movie_gross.csv.gz')
+
+
+# In[5]:
+
+
+bom
+
+
+# In[6]:
+
+
+tmdb = pd.read_csv('Downloads/dsc-project-template-template-mvp (1)/dsc-project-template-template-mvp/zippedData/tmdb.movies.csv.gz')
+
+
+# In[7]:
+
+
+tmdb
+
+
+# In[ ]:
+
+
+
+
